ee211e08b4100577fd3e8c5cc9cd2f091c1012df
Chris41g <chriswhite41g@gmail.com>
2011-10-07 22:47:03 -0500

    fixed module script

--- scripts/update_modules.sh
+++ scripts/update_modules.sh
@@ -42,7 +42,7 @@ if [ "$1" == "cp" ] || [ "$1" == "copy" ] ; then
 	COPY_WITH_ECHO "bluetooth/bthid/bthid.ko" "bthid.ko"
 	COPY_WITH_ECHO "samsung/j4fs/j4fs.ko" "j4fs.ko"
 	COPY_WITH_ECHO "scsi/scsi_wait_scan.ko" "scsi_wait_scan.ko"
-	#COPY_WITH_ECHO "net/wireless/bcm4330/dhd.ko" initramfs/lib/modules/
+	COPY_WITH_ECHO "net/wireless/bcm4330/dhd.ko" initramfs/lib/modules/
 	COPY_WITH_ECHO "samsung/vibetonz/vibrator.ko" "vibrator.ko"
 	COPY_WITH_ECHO "staging/westbridge/astoria/switch/cyasswitch.ko" "cyasswitch.ko"
 	
