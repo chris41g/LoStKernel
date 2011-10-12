2071b857a221ddb566f03b7c5bb0a6cc378db975
Chris41g <chriswhite41g@gmail.com>
2011-10-11 22:50:55 -0500

    script debugging

--- initramfs/sbin/userscript.sh
+++ initramfs/sbin/userscript.sh
@@ -1,15 +1,25 @@
 #!/system/bin/sh -x
 # Remount filesystems RW
-# exec > /data/local/userscript.log 2>&1
+exec > /data/local/userscript.log 2>&1
 /sbin/busybox mount -o remount,rw / /
 /sbin/busybox mount -o remount,rw /dev/block/mmcblk0p9 /system
 FILESIZE=$(/sbin/busybox cat /data/local/LoStKernel-Ver|wc -c)
-/sbin/busybox test $FILESIZE -ge 10000 && /sbin/busybox rm /data/local/LoStKernel-Ver
+if /sbin/busybox test "$FILESIZE" -ge "10000"; then
+	/sbin/busybox rm /data/local/LoStKernel-Ver
+fi
+
 /sbin/busybox echo "Starting Boot Script - " $(/sbin/busybox date) >> /data/local/LoStKernel-Ver
 /sbin/busybox echo $(/sbin/busybox uname -a) >> /data/local/LoStKernel-Ver
-/sbin/busybox test ! -e "/system/xbin/busybox" && installbb()
-/sbin/busybox test ! -e "/system/bin/su" && /sbin/busybox test ! -e "/system/xbin/su" && installsu()
-# touch /data/local/LoStKernel-Ver
+
+if /sbin/busybox test ! -e "/system/xbin/busybox"; then
+installbb()
+fi
+
+if /sbin/busybox test ! -e "/system/bin/su"; then
+	if /sbin/busybox test ! -e "/system/xbin/su"; then
+	installsu()
+	fi
+fi
 
 installbb() 
 {
@@ -30,28 +40,60 @@ installsu()
 /sbin/busybox rm -rf /sbin/su
 /sbin/busybox echo "Installed SU Binary" >> /data/local/LoStKernel-Ver
 }
-/sbin/busybox test ! -f "/system/app/Superuser.apk" && /sbin/busybox test ! -f "/data/app/Superuser.apk" && /sbin/busybox test ! -f "/data/app/com.noshufou.android.su"* && /sbin/busybox cp /sbin/superuser.apk /data/app/superuser.apk && /sbin/busybox echo "Installed SuperUser.apk" >> /data/local/LoStKernel-Ver
+
+if /sbin/busybox test ! -f "/system/app/Superuser.apk"; then
+	if /sbin/busybox test ! -f "/data/app/Superuser.apk"; then
+		if /sbin/busybox test ! -f "/data/app/com.noshufou.android.su"*; then
+			/sbin/busybox cp /sbin/superuser.apk /data/app/superuser.apk
+			/sbin/busybox echo "Installed SuperUser.apk" >> /data/local/LoStKernel-Ver
+		fi
+	fi
+fi
 /sbin/busybox rm /sbin/superuser.apk
 # Enable init.d support
-/sbin/busybox test -d "/system/etc/init.d" &&  /sbin/busybox run-parts /system/etc/init.d && /sbin/busybox echo "Init.d Started" >> /data/local/LoStKernel-Ver && sync
+if /sbin/busybox test -d "/system/etc/init.d"; then
+	/sbin/busybox run-parts /system/etc/init.d
+	/sbin/busybox echo "Init.d Started" >> /data/local/LoStKernel-Ver
+	sync
+fi
 
 #setup proper passwd and group files for 3rd party root access
 # Thanks DevinXtreme
-/sbin/busybox test ! -f "/system/etc/passwd" &&	/sbin/busybox echo "root::0:0:root:/data/local:/system/bin/sh" > /system/etc/passwd && /sbin/busybox chmod 0666 /system/etc/passwd
-/sbin/busybox test ! -f "/system/etc/group" && /sbin/busybox echo "root::0:" > /system/etc/group && /sbin/busybox chmod 0666 /system/etc/group
+if /sbin/busybox test ! -f "/system/etc/passwd"; then
+	/sbin/busybox echo "root::0:0:root:/data/local:/system/bin/sh" > /system/etc/passwd
+	/sbin/busybox chmod 0666 /system/etc/passwd
+fi
+
+if /sbin/busybox test ! -f "/system/etc/group"; then
+	/sbin/busybox echo "root::0:" > /system/etc/group
+	/sbin/busybox chmod 0666 /system/etc/group
+fi
 
 # fix busybox DNS while system is read-write
-/sbin/busybox test ! -f "/system/etc/resolv.conf" && /sbin/busybox echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf && /sbin/busybox echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
+if /sbin/busybox test ! -f "/system/etc/resolv.conf"; then
+	/sbin/busybox echo "nameserver 8.8.8.8" >> /system/etc/resolv.conf
+	/sbin/busybox echo "nameserver 8.8.4.4" >> /system/etc/resolv.conf
+fi
 
 # patch to prevent certain malware apps
-/sbin/busybox test -f "/system/bin/profile" && /sbin/busybox rm /system/bin/profile
-/sbin/busybox touch /system/bin/profile && /sbin/busybox echo "Malware Patch" >> /data/local/LoStKernel-Ver
+if /sbin/busybox test -f "/system/bin/profile"; then
+	/sbin/busybox rm /system/bin/profile
+fi
+
+/sbin/busybox touch /system/bin/profile
+/sbin/busybox echo "Malware Patch" >> /data/local/LoStKernel-Ver
 /sbin/busybox chmod 644 /system/bin/profile
 # remove bad BB installs
 /sbin/busybox ls -1 /system/bin | while read line
   do
-    /sbin/busybox test "$(/sbin/busybox basename `/sbin/busybox readlink /system/bin/$line`)" = "busybox" && /sbin/busybox rm /system/bin/${line} && /sbin/busybox echo "Removing Bad BusyBox Install - /system/bin/"${line} >> /data/local/LoStKernel-Ver
-    /sbin/busybox test "$(/sbin/busybox basename `/sbin/busybox readlink /system/bin/$line`)" = "recovery" && /sbin/busybox rm /system/bin/${line} && /sbin/busybox echo "Removing Bad BusyBox Install - /system/bin/"${line} >> /data/local/LoStKernel-Ver
+    if /sbin/busybox test "$(/sbin/busybox basename `/sbin/busybox readlink /system/bin/$line`)" = "busybox"; then
+		/sbin/busybox rm /system/bin/${line}
+		/sbin/busybox echo "Removing Bad BusyBox Install - /system/bin/"${line} >> /data/local/LoStKernel-Ver
+	fi
+    if /sbin/busybox test "$(/sbin/busybox basename `/sbin/busybox readlink /system/bin/$line`)" = "recovery"; then
+		/sbin/busybox rm /system/bin/${line}
+		/sbin/busybox echo "Removing Bad BusyBox Install - /system/bin/"${line} >> /data/local/LoStKernel-Ver
+	fi
   done
 # Relink Toolbox
 
